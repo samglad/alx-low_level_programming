af20163bbb8c52c3a68a9c119f9e8c085291e077
samglad <sammyglad1@gmail.com>
2023-09-07 14:44:07 -0700

    program that prints text to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - This program prints text to the standard error
+ * Return: This reurns 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,
+	2015-10-19\n");
+	return (1);
