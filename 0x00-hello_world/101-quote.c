1f09dec911b824219ccc85076221e220fd956199
samglad <sammyglad1@gmail.com>
2023-09-07 14:57:26 -0700

    program that prints text to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,5 @@ i#include <stdio.h>
 int main(void)
 {
 	write(2, "\"and that piece of art is useful\" - Dora Korpar, 
-			2015-10-19\n", sizeof("\"and that piece of art is 
-			useful\" - Dora Korpar, 2015-10-19\n"));
+	2015-10-19\n", 59);
 	return (1);
