b68c304399582178ed5f018c40b5024ce1c3ea75
samglad <sammyglad1@gmail.com>
2023-09-07 06:37:54 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This prints text
+ * Return: This returns 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	puts("");
+	return (0);
+}
