cb9afd62e9a79dbc5c049b43bdd64be5cedcb5ec
samglad <sammyglad1@gmail.com>
2023-09-07 06:51:37 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 
 /**
  * main - This program prints text
- * Return: this returns 0
+ * Return - this returns 0
  */
 
-int main(0)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
