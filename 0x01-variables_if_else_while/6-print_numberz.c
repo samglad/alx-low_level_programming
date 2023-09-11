dbd0bd4c4b0499a7761abca6b3b4bb807944d69c
samglad <sammyglad1@gmail.com>
2023-09-10 18:00:37 -0700

    prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Description: prints all single digit numbers of base 10 starting from 0
+  * Return: Always returns 0
+  */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 9; n++)
+	{
+		putchar(n + '0');
+	}
+	putchar('\n');
+	return (0);
+}
