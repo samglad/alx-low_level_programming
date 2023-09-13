bfeefae229303308dc687dd3da1a31bd9a68fdd8
samglad <sammyglad1@gmail.com>
2023-09-13 13:01:18 -0700

    a function that prints the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
   * main - Entry point
-  * Description: A program that prints the alphabet in lowercase
+  * Description: A function that prints the alphabet, in lowercase
   * Return: Always returns 0
   */
 
