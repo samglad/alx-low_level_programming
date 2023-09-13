c3b2f62782ccea96c6a96e766500041c2b3becc5
samglad <sammyglad1@gmail.com>
2023-09-13 12:48:21 -0700

    a function that prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include main.h
+
+/**
+  * main - Entry point
+  * Description: A program that prints the alphabet in lowercase
+  * Return: Always returns 0
+  */
+
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n')
+}
