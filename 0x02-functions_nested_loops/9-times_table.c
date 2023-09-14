ca8f5c53882a8e9b10ab881e69f2c5d48ebfa413
samglad <sammyglad1@gmail.com>
2023-09-13 17:02:07 -0700

    function that print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,12 +26,12 @@ void times_table(void)
 			{
 				_putchar(' ');
 			}
-			}
 			else
 			{
 				_putchar((product / 10) + '0');
-			}
+ 			}
 			_putchar((product % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
