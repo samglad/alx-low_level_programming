b6fbf39f7e4574523a1874b96f5ed47712964ac9
samglad <sammyglad1@gmail.com>
2023-09-14 20:33:39 -0700

    program converts a date to the day of year

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -12,10 +12,11 @@ int main(void)
     int month;
     int day;
     int year;
+    int convert_day(int month, int day);
 
-    month = 4;
-    day = 01;
-    year = 1997;
+    month = 2;
+    day = 29;
+    year = 2000;
 
     printf("Date: %02d/%02d/%04d\n", month, day, year);
 
