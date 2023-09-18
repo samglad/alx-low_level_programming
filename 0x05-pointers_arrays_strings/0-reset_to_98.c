d6f644d92c3cd0a1ff75c15237646a82f366f6f7
samglad <sammyglad1@gmail.com>
2023-09-18 16:06:38 -0700

    accepts an int pointer n

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - Entry point
+  * Description: A function that takes a pointer to an int as parameter
+  * and updates the value it points to to 98
+  * @n: input parameter
+  * Return: returns n
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
